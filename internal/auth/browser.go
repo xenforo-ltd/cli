6b0f1fa4e968d434535107fd976e641c39913bf4
Jeremy P <git@jerpr.com>
2026-03-29 15:01:54 -0700

    Harden `OpenBrowser` function

--- internal/auth/browser.go
+++ internal/auth/browser.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"net/url"
 	"os/exec"
 	"runtime"
 
@@ -9,16 +10,21 @@ import (
 )
 
 // OpenBrowser opens a URL in the user's default browser.
-func OpenBrowser(ctx context.Context, url string) error {
+func OpenBrowser(ctx context.Context, urlStr string) error {
+	u, err := url.Parse(urlStr)
+	if err != nil || (u.Scheme != "http" && u.Scheme != "https") {
+		return clierrors.Newf(clierrors.CodeInternal, "invalid URL: %s", urlStr)
+	}
+
 	var cmd *exec.Cmd
 
 	switch runtime.GOOS {
 	case "darwin":
-		cmd = exec.CommandContext(ctx, "open", url)
+		cmd = exec.CommandContext(ctx, "open", u.String())
 	case "linux":
-		cmd = exec.CommandContext(ctx, "xdg-open", url)
+		cmd = exec.CommandContext(ctx, "xdg-open", u.String())
 	case "windows":
-		cmd = exec.CommandContext(ctx, "rundll32", "url.dll,FileProtocolHandler", url)
+		cmd = exec.CommandContext(ctx, "rundll32", "url.dll,FileProtocolHandler", u.String())
 	default:
 		return clierrors.Newf(clierrors.CodeInternal, "unsupported platform: %s", runtime.GOOS)
 	}
